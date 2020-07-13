1bb0da2fb7d2addd9942d2376c6436fd4d112ecb
chen_pupil <chen_pupil@wotian.com>
2020-07-13 15:52:05 +0800

    陈杏昌在20200713下午15：51在启动类加了注释

--- src/main/java/com/chen/Starter.java
+++ src/main/java/com/chen/Starter.java
@@ -5,6 +5,9 @@ import com.chen.entity.Book;
 import com.chen.entity.User;
 import com.jfinal.server.undertow.UndertowServer;
 
+/**
+ * 项目启动类
+ */
 public class Starter {
     public static void main(String[] args) {
         UndertowServer.start(JfinalConfig.class);
